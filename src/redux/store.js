e8b076f55cca5908e544bf0b32348928161a9f65
doziechuks <doziechuks1010@gmail.com>
2023-01-04 23:30:21 +0100

    rendered logger only on development

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,10 @@ import { persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-const middleWares = [thunk, logger];
+const middleWares = [thunk];
+if (process.env.NODE_ENV === "development") {
+  middleWares.push(logger);
+}
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(...middleWares));
 export const persist = persistStore(store);
